1184aa89dfe2fb510878b1af298ec75743ecbd49
Nikodem Kusiak <nikodemkusiak@Nikodems-MacBook-Air.local>
2024-03-21 14:29:42 +0100

    Refresh repo

---
+++ football-api/node.js
@@ -0,0 +1,24 @@
+// const axios = require('axios');
+
+// const fetchData = async () => {
+//   const options = {
+//     method: 'GET',
+//     url: 'https://v3.football.api-sports.io/',
+//     headers: {
+//       'X-RapidAPI-Key': '257a4f45eda9f4097cb86ffdf94bbc5b',
+//       'X-RapidAPI-Host': 'v3.football.api-sports.io'
+//     }
+//   };
+
+//   try {
+//     const response = await axios.request(options);
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// // Call the asynchronous function
+// fetchData();
+
+
